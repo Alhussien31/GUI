ad3003e9b79e51dd1efec39f0bfb07fab633477e
ALHUSSIEN <ALHUSSIEN@DESKTOP-186M9MT>
2020-04-07 17:04:46 +0300

    GUI for students name

---
+++ Gui1/src/GuiStudensName.java
@@ -0,0 +1,93 @@
+/*the site below was used to help build the code
+ * https://docs.oracle.com/javase/tutorial/uiswing/components/list.html*/
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+ 
+public class GuiStudensName extends JPanel {
+    private boolean DEBUG = false;
+ 
+    public GuiStudensName() {
+        super(new GridLayout(1,0));
+ 
+        String[] columnNames = {"First Name",
+                                "Last Name"
+                                };
+ 
+        Object[][] data = {
+        {"Alhuusien ", "Al-Sharman"
+         },
+        {"Alwai", "Al-Musabi"
+        },
+        {"Abdulrahman ", "Al-Bahri"
+         },
+        {"Mohammed", "Al-Qahtani"
+         },
+        {"Nawaf ", "Al-Wadi"
+        },{"Riyad","Al-Amri"}
+        };
+ 
+        final JTable table = new JTable(data, columnNames);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+ 
+        if (DEBUG) {
+            table.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    printDebugData(table);
+                }
+            });
+        }
+ 
+       
+        JScrollPane scrollPane = new JScrollPane(table);
+ 
+        add(scrollPane);
+    }
+ 
+    private void printDebugData(JTable table) {
+        int numRows = table.getRowCount();
+        int numCols = table.getColumnCount();
+        javax.swing.table.TableModel model = table.getModel();
+ 
+        System.out.println("Value of data: ");
+        for (int i=0; i < numRows; i++) {
+            System.out.print("    row " + i + ":");
+            for (int j=0; j < numCols; j++) {
+                System.out.print("  " + model.getValueAt(i, j));
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+    }
+ 
+   
+    private static void createAndShowGUI() {
+       
+        JFrame frame = new JFrame("SimpleTableDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        GuiStudensName newContentPane = new GuiStudensName();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+ 
+        
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+    	
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
